871206c229e72c2094bd2eb392f2e6e3764e8271
Mersad <mosaybimersvd020@gmail.com>
2025-12-29 20:26:41 +0330

    filterTab added and left and right slider become smart. courseType Fixed and courseData and courseType import to landingPage

--- farahDars-TS/src/Component/landing/SliderWithCat.tsx
+++ farahDars-TS/src/Component/landing/SliderWithCat.tsx
@@ -3,53 +3,66 @@ import {useState,useRef} from "react"
 
 //course Card
 import CourseCard from "../product_Card/courseCard"
-import type { courseList } from "../product_Card/courseCard";
+import type { courseDataList } from "../../Page/landing/landingPage";
+type courseListProps = {
+   courseList:courseDataList[];
+}
+type tab = {
+   tabIndex:number,
+   technology:string
+}
 
-
-const SliderWithCat = ()=>{
+const SliderWithCat = ({courseList}:courseListProps)=>{
  
-   const sliderTab :string[] =  ["همه","برنامه نویسی","هوش مصنوعی"]
-   
-   const [borderActive,setBorderActive] = useState<number>(0);
+  //// slider tab 
+   const sliderTab :string[] =  ["همه","برنامه نویسی","هوش مصنوعی"];
 
-   const galPos = useRef<HTMLDivElement>(null);
+   const tabHandler =(index:number,label:string)=>{
+      const tech = label === "همه" ? "": label;   
+    setTab({...tab,tabIndex : index ,technology:tech});
+   }
+   
+   const [tab,setTab] = useState<tab>({
+      tabIndex:0,
+      technology:""
+   });
+   // console.log("tab",tab);
+   const filteredCourse = courseList.filter((cr)=>{
+
+       if(tab.technology && !cr.technology.includes(tab.technology)) return false
+
+       return true
+   })
+
+  ///// slider left and right handler 
+   const galPosition = useRef<HTMLDivElement>(null);
     let galpos :number = 0;
+    let perMove:number = 21;
+    let courseTotal:number = filteredCourse.length;
+    let sliderLimit:number = courseTotal*21 - 64;
     
    const sliderLeftHandler = ():void=>{
-       if(!galPos.current) return
-       if( galpos > -146 ){
-         galpos = galpos -21;
+       if(!galPosition.current) return
+       if( galpos > -sliderLimit ){
+         galpos = galpos -perMove;
        }
-       else if(galpos = -147){
+       else if(galpos = -sliderLimit){
          galpos = 0;
        }
-       galPos.current.style.right = `${galpos}%`
+       galPosition.current.style.right = `${galpos}%`
    }
 
    const sliderRightHandler = ():void=>{
-       if(!galPos.current) return
+       if(!galPosition.current) return
        if( 0 <= galpos){
-         galpos = -147;
+         galpos = -sliderLimit;
        }
-       else if(galpos > -150){
-         galpos = galpos + 21
+       else if(galpos > -(sliderLimit + 4)){
+         galpos = galpos + perMove;
        }
-       galPos.current.style.right = `${galpos}%`
+       galPosition.current.style.right = `${galpos}%`
    }
 
-
-   const courseList :courseList[] = [
-   {id:1,title:"آموزش برنامه نویسی Matlab",teacherName:"سید مصطفی کلامی هریس",courseTime:9,price:790000,disCount:80,ImageAddress:"https://faradars.org/_next/image?url=https%3A%2F%2Ffaradars.org%2Fwp-content%2Fuploads%2F2024%2F04%2F17%2Fmvrma92021.svg&w=1920&q=75" },
-   {id:2,title:"آموزش برنامه نویسی Matlab",teacherName:"سید مصطفی کلامی هریس",courseTime:9,price:790000,disCount:100,ImageAddress:"https://faradars.org/_next/image?url=https%3A%2F%2Ffaradars.org%2Fwp-content%2Fuploads%2F2024%2F04%2F17%2Fmvrma92021.svg&w=1920&q=75" },
-   {id:3,title:"آموزش برنامه نویسی Matlab",teacherName:"سید مصطفی کلامی هریس",courseTime:9,price:790000,disCount:80,ImageAddress:"https://faradars.org/_next/image?url=https%3A%2F%2Ffaradars.org%2Fwp-content%2Fuploads%2F2024%2F04%2F17%2Fmvrma92021.svg&w=1920&q=75" },
-   {id:4,title:"آموزش برنامه نویسی Matlab",teacherName:"سید مصطفی کلامی هریس",courseTime:9,price:790000,disCount:80,ImageAddress:"https://faradars.org/_next/image?url=https%3A%2F%2Ffaradars.org%2Fwp-content%2Fuploads%2F2024%2F04%2F17%2Fmvrma92021.svg&w=1920&q=75" },
-   {id:5,title:"آموزش برنامه نویسی Matlab",teacherName:"سید مصطفی کلامی هریس",courseTime:9,price:790000,disCount:80,ImageAddress:"https://faradars.org/_next/image?url=https%3A%2F%2Ffaradars.org%2Fwp-content%2Fuploads%2F2024%2F04%2F17%2Fmvrma92021.svg&w=1920&q=75" },
-   {id:6,title:"آموزش برنامه نویسی Matlab",teacherName:"سید مصطفی کلامی هریس",courseTime:9,price:790000,disCount:90,ImageAddress:"https://faradars.org/_next/image?url=https%3A%2F%2Ffaradars.org%2Fwp-content%2Fuploads%2F2024%2F04%2F17%2Fmvrma92021.svg&w=1920&q=75" },
-   {id:7,title:"آموزش برنامه نویسی Matlab",teacherName:"سید مصطفی کلامی هریس",courseTime:9,price:790000,disCount:50,ImageAddress:"https://faradars.org/_next/image?url=https%3A%2F%2Ffaradars.org%2Fwp-content%2Fuploads%2F2024%2F04%2F17%2Fmvrma92021.svg&w=1920&q=75" },
-   {id:8,title:"آموزش برنامه نویسی Matlab",teacherName:"سید مصطفی کلامی هریس",courseTime:9,price:790000,disCount:80,ImageAddress:"https://faradars.org/_next/image?url=https%3A%2F%2Ffaradars.org%2Fwp-content%2Fuploads%2F2024%2F04%2F17%2Fmvrma92021.svg&w=1920&q=75" },
-   {id:9,title:"آموزش برنامه نویسی Matlab",teacherName:"سید مصطفی کلامی هریس",courseTime:9,price:790000,disCount:80,ImageAddress:"https://faradars.org/_next/image?url=https%3A%2F%2Ffaradars.org%2Fwp-content%2Fuploads%2F2024%2F04%2F17%2Fmvrma92021.svg&w=1920&q=75" },
-   {id:10,title:"آموزش برنامه نویسی Matlab",teacherName:"سید مصطفی کلامی هریس",courseTime:9,price:790000,disCount:80,ImageAddress:"https://faradars.org/_next/image?url=https%3A%2F%2Ffaradars.org%2Fwp-content%2Fuploads%2F2024%2F04%2F17%2Fmvrma92021.svg&w=1920&q=75" }
-   ]
 
    return (
       <div className=" w-full h-[506.42px]">
@@ -79,9 +92,9 @@ const SliderWithCat = ()=>{
          <div className=" flex flex-row gap-5 w-full h-[8%] border-b border-[gray] px-3">
             {sliderTab.map((label,index)=>(
              <button key={index}
-              onClick={()=>setBorderActive(index)} 
+              onClick={()=>tabHandler(index,label)} 
               className={`text-[15px] border-[blue]/60 hover:bg-[gray]/20 h-full transition-colors duration-300
-                ${borderActive === index ?
+                ${tab.tabIndex === index ?
                   "border-b-2 text-[blue]"
                   :
                   ""
@@ -95,10 +108,10 @@ const SliderWithCat = ()=>{
 
          {/*gal*/}         
          <div className="Slider w-full h-[68%] overflow-hidden max-lg:overflow-x-scroll hide-scrollbar">
-           <div ref={galPos}  className="gal w-[3000px] h-full
+           <div ref={galPosition}  className="gal w-[3000px] h-full
             flex flex-row items-center gap-5 p-1 pt-5 transition-all duration-500 ease-in-out
             relative right-[0%] ">
-               {courseList.map((card)=>(
+               {filteredCourse.map((card)=>(
                   <CourseCard
                     key={card.id}
                     id={card.id}
--- farahDars-TS/src/Page/landing/landingPage.tsx
+++ farahDars-TS/src/Page/landing/landingPage.tsx
@@ -2,7 +2,28 @@ import Hero from "./Hero"
 import LandingReport from "./LandingReport"
 import SliderWithCat from "../../Component/landing/SliderWithCat"
 
+import type { courseList } from "../../Component/product_Card/courseCard";
+export type courseDataList = courseList &{
+    technology:string[],
+    courseLevel:string
+}
+
 const landingPage = ()=>{
+
+   // course data
+     const courseList :courseDataList[] = [
+     {id:1,technology:["هوش مصنوعی"],courseLevel:"متوسط",title:"آموزش برنامه نویسی Matlab",teacherName:"سید مصطفی کلامی هریس",courseTime:9,price:790000,disCount:80,ImageAddress:"https://faradars.org/_next/image?url=https%3A%2F%2Ffaradars.org%2Fwp-content%2Fuploads%2F2024%2F04%2F17%2Fmvrma92021.svg&w=1920&q=75" },
+     {id:2,technology:["هوش مصنوعی"],courseLevel:"متوسط",title:"آموزش برنامه نویسی Matlab",teacherName:"سید مصطفی کلامی هریس",courseTime:9,price:790000,disCount:100,ImageAddress:"https://faradars.org/_next/image?url=https%3A%2F%2Ffaradars.org%2Fwp-content%2Fuploads%2F2024%2F04%2F17%2Fmvrma92021.svg&w=1920&q=75" },
+     {id:3,technology:["هوش مصنوعی"],courseLevel:"متوسط",title:"آموزش برنامه نویسی Matlab",teacherName:"سید مصطفی کلامی هریس",courseTime:9,price:790000,disCount:80,ImageAddress:"https://faradars.org/_next/image?url=https%3A%2F%2Ffaradars.org%2Fwp-content%2Fuploads%2F2024%2F04%2F17%2Fmvrma92021.svg&w=1920&q=75" },
+     {id:4,technology:["برنامه نویسی"],courseLevel:"متوسط",title:"آموزش برنامه نویسی Matlab",teacherName:"سید مصطفی کلامی هریس",courseTime:9,price:790000,disCount:80,ImageAddress:"https://faradars.org/_next/image?url=https%3A%2F%2Ffaradars.org%2Fwp-content%2Fuploads%2F2024%2F04%2F17%2Fmvrma92021.svg&w=1920&q=75" },
+     {id:5,technology:["برنامه نویسی"],courseLevel:"متوسط",title:"آموزش برنامه نویسی Matlab",teacherName:"سید مصطفی کلامی هریس",courseTime:9,price:790000,disCount:80,ImageAddress:"https://faradars.org/_next/image?url=https%3A%2F%2Ffaradars.org%2Fwp-content%2Fuploads%2F2024%2F04%2F17%2Fmvrma92021.svg&w=1920&q=75" },
+     {id:6,technology:["هوش مصنوعی"],courseLevel:"متوسط",title:"آموزش برنامه نویسی Matlab",teacherName:"سید مصطفی کلامی هریس",courseTime:9,price:790000,disCount:90,ImageAddress:"https://faradars.org/_next/image?url=https%3A%2F%2Ffaradars.org%2Fwp-content%2Fuploads%2F2024%2F04%2F17%2Fmvrma92021.svg&w=1920&q=75" },
+     {id:7,technology:["هوش مصنوعی"],courseLevel:"متوسط",title:"آموزش برنامه نویسی Matlab",teacherName:"سید مصطفی کلامی هریس",courseTime:9,price:790000,disCount:50,ImageAddress:"https://faradars.org/_next/image?url=https%3A%2F%2Ffaradars.org%2Fwp-content%2Fuploads%2F2024%2F04%2F17%2Fmvrma92021.svg&w=1920&q=75" },
+     {id:8,technology:["هوش مصنوعی","برنامه نویسی"],courseLevel:"متوسط",title:"آموزش برنامه نویسی Matlab",teacherName:"سید مصطفی کلامی هریس",courseTime:9,price:790000,disCount:80,ImageAddress:"https://faradars.org/_next/image?url=https%3A%2F%2Ffaradars.org%2Fwp-content%2Fuploads%2F2024%2F04%2F17%2Fmvrma92021.svg&w=1920&q=75" },
+     {id:9,technology:["هوش مصنوعی","برنامه نویسی"],courseLevel:"متوسط",title:"آموزش برنامه نویسی Matlab",teacherName:"سید مصطفی کلامی هریس",courseTime:9,price:790000,disCount:80,ImageAddress:"https://faradars.org/_next/image?url=https%3A%2F%2Ffaradars.org%2Fwp-content%2Fuploads%2F2024%2F04%2F17%2Fmvrma92021.svg&w=1920&q=75" },
+     {id:10,technology:["هوش مصنوعی","برنامه نویسی"],courseLevel:"متوسط",title:"آموزش برنامه نویسی Matlab",teacherName:"سید مصطفی کلامی هریس",courseTime:9,price:790000,disCount:80,ImageAddress:"https://faradars.org/_next/image?url=https%3A%2F%2Ffaradars.org%2Fwp-content%2Fuploads%2F2024%2F04%2F17%2Fmvrma92021.svg&w=1920&q=75" }
+     ]
+
     return (
         <div className="  w-full bg-gradient-to-b from-sky-200 via-gray-200 to-white ">
           <div className=" min-2xl:container min-2xl:mx-auto flex flex-col place-items-center gap-5 ">
@@ -11,10 +32,10 @@ const landingPage = ()=>{
           </section>
           <LandingReport/>
            <div className="w-[80%]">
-          <SliderWithCat/>
+          <SliderWithCat courseList={courseList}/>
            </div>
            <div className="w-[80%]">
-          <SliderWithCat/>
+          <SliderWithCat courseList={courseList} />
            </div>
 
           </div>
